01432117b0fd005e6b3fd5439f8f4923021854f0
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-04-03 07:24:51 -0300

    chore: organizing renderer files

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './home'
-import './index.css'
+import Home from './pages/home'
+import './styles/index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
 	<StrictMode>

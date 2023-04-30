5ab39bd6bba59553919bafde9f2a784bfe716b7b
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-04-30 16:59:28 -0300

    chore: updating configs

--- main/index.ts
+++ main/index.ts
@@ -1,11 +1,8 @@
 import { app, BrowserWindow, Menu, Tray } from 'electron'
 import { resolve } from 'path'
-import reload from './reload'
 import { dirPath, getPlatformParams, getWindowConfig } from './utils'
 
 app.whenReady().then(() => {
-	setTimeout(() => reload(resolve(dirPath, 'watch')), 5000)
-
 	const [ext, traySize, showOpenOpt] = getPlatformParams
 	const iconPath = (number: number) =>
 		resolve(dirPath, 'src', 'assets', `icon${number}.${ext}`)
@@ -26,7 +23,7 @@ app.whenReady().then(() => {
 		menuTemplate.splice(0, 0, { label: 'Open', click: () => window.show() })
 	tray.setContextMenu(Menu.buildFromTemplate(menuTemplate))
 
-	window.loadURL('http://localhost:5173').then(() => {
+	window.loadURL('http://localhost:3000').then(() => {
 		window.show()
 	})
 })

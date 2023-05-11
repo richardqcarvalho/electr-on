59625f31b1fd502dc87a008c256fac06ef131a8c
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-05-11 12:24:09 -0300

    chore: separating tsconfig to electron & next

--- main/index.ts
+++ main/index.ts
@@ -5,7 +5,7 @@ import { dirPath, getPlatformParams, getWindowConfig } from './utils'
 app.whenReady().then(() => {
   const [ext, traySize, showOpenOpt] = getPlatformParams
   const iconPath = (number: number) =>
-    resolve(dirPath, 'src', 'assets', `icon${number}.${ext}`)
+    resolve(dirPath, 'assets', `icon${number}.${ext}`)
   const windowConfig = getWindowConfig(500, 500)
   const window = new BrowserWindow({
     icon: iconPath(64),
